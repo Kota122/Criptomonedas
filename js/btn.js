3f7ff2ae32c18d89b6c53ef747c0218946212eb4
Kota122 <67023623+Kota122@users.noreply.github.com>
2020-07-03 02:05:09 -0400

    Add files via upload

---
+++ js/btn.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 300) {
+            $('#boton').fadeIn();
+        } else {
+            $('#boton').fadeOut();
+        }
+    });
+
+    $('#boton').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
+});
